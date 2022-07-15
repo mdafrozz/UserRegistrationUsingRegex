23dadf3d6ced457ab3a26e74da1ff69d211c22d9
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-07-15 09:39:46 +0530

    Add: User need to enter a valid First Name

---
+++ UserRegistration.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.bridgelabz.main;
+
+/**
+ * @author MD_AFROZ
+ *
+ */
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("*********Welcome to user registration with regex***********");
+		
+		UserImplements user = new UserImplements();
+		user.validateFirstName();
+
+	}
+}
